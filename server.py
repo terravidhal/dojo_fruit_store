b262f23dee4ec50f4075dfd07037d696b7c92617
terravidhal <infiniterraetincelle@gmail.com>
2023-09-03 06:49:50 +0100

    update dojo_fruit by terra

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from flask import Flask, render_template, request, redirect
+from datetime import datetime 
+
+
 app = Flask(__name__)  
 
 @app.route('/')         
@@ -7,12 +10,22 @@ def index():
 
 @app.route('/checkout', methods=['POST'])         
 def checkout():
-    print(request.form)
-    return render_template("checkout.html")
+    totalItems= int(request.form['strawberry'])+int(request.form['raspberry'])+int(request.form['apple'])
+    current_date_time = datetime.now().strftime("%B %dth %Y %I:%M:%S %p")
+    print(f"Charging {request.form['first_name']} for {totalItems} fruits")
+    return render_template("checkout.html",users_infos = request.form, total_items = totalItems ,currentDatetime = current_date_time  )
 
 @app.route('/fruits')         
 def fruits():
     return render_template("fruits.html")
+
+
+@app.errorhandler(404)  # we specify in parameter here the type of error, here it is 404
+def page_not_found(
+    error,
+):  # (error) is important because it recovers the instance of the error that was thrown
+    return f"<h2 style='text-align:center;padding-top:40px'>Sorry! No response. Try again</h2>"
+
 
 if __name__=="__main__":   
     app.run(debug=True)    
